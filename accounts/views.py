b56bd01735c434f4a8012fd07d377650acc9a59e
Bejoy Mathew <bejoygm@gmail.com>
2018-01-11 19:22:39 +0530

    custom passwordless auth backend + custom user model

--- accounts/views.py
+++ accounts/views.py
@@ -24,6 +24,7 @@ def send_login_email(request):
     return redirect('/')
 
 def login(request):
-    auth.authenticate(uid=request.GET.get('token'))
-    auth.login(request, user)
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
     return redirect('/')
