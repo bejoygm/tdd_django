b0bea0d7725bfec373f8c6c8c1f567880df183cd
Bejoy Mathew <bejoygm@gmail.com>
2018-01-11 19:32:06 +0530

    fixed smtp email

--- superlists/settings.py
+++ superlists/settings.py
@@ -147,7 +147,7 @@ LOGGING = {
 }
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bejoygm@gmail.com'
+EMAIL_HOST_USER = 'superlists22@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
